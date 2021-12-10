81236db4151aef360ee926b179bb4c7f69be3bdc
metehaninis <metehan-330@hotmail.com>
2021-12-10 20:31:58 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    //T yerine ne gönderirsek onu alır(product,category)
+    //generic constraint     T yi sınırlandırıyoruz   class: referans tip olabilir demek
+    //herhangibir class da yazamasın  - T bir referans tip olmalı ya IEntity olmalı ya da ondan implemente olan birşey olabilir
+    //new lenebilir olamalı yani artık interface olan IEntity de girilemez
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        //linq yazdığımız kısım expression getall içerisinde kullanabilmemizi sağlar - filtre vermeyebilirsin demek yani mantıken tüm datayı istiyor
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);          //interface metotları default public tir ancak kendisi değil
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,68 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _product;
+        public InMemoryProductDal()  //bellekte referans aldığı zaman çalışacak olan blok
+        {
+            _product = new List<Product> {
+            new Product{ProductId=1,CategoryId=1,ProductName="Bardak",UnitPrice=15,UnitsInStock=15},
+            new Product{ProductId=2,CategoryId=1,ProductName="Kamera",UnitPrice=500,UnitsInStock=3},
+            new Product{ProductId=3,CategoryId=2,ProductName="Telefon",UnitPrice=1500,UnitsInStock=2},
+            new Product{ProductId=4,CategoryId=2,ProductName="Klavye",UnitPrice=150,UnitsInStock=65},
+            new Product{ProductId=5,CategoryId=2,ProductName="Fare",UnitPrice=85,UnitsInStock=1},
+            };
+        }
+        public void Add(Product product)
+        {
+            _product.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            //LINQ - Language Integrated Query
+            //tek tek dolaşır foreach mantığı -firstordefault ile aynı
+            Product productToDelete = _product.SingleOrDefault(p => p.ProductId == product.ProductId);
+            _product.Remove(productToDelete);
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _product;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllCategory(int categoryId)
+        {
+            return _product.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            //Gönderdiğim ürün ıd sine sahip olan listedeki ürünü bul
+            Product productToUpdate = _product.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+    }
+}
