903f544b5e39bd47e14f4a654115b3fc4ae20c93
metehaninis <metehan-330@hotmail.com>
2021-12-29 18:24:57 +0300

    Add project files.

---
+++ Core/Utilities/Business/BusinessRules.cs
@@ -0,0 +1,24 @@
+﻿using Core.Utilities.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Business
+{
+    public class BusinessRules
+    {
+        public static IResult Run(params IResult[] logics) //istediğimiz kadar parametre gönderebiliriz
+        {
+            foreach (var logic in logics)
+            {
+                if (!logic.Success)
+                {
+                    return logic;       //parametre ile gönderilen iş kurallarından başarısız olanı business a haberdar ediyoruz
+                }
+            }
+            return null;
+        }
+    }
+}
